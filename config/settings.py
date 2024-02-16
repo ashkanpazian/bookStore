b37454572ce72fa942f640545483ab96fd10235f
ashkanpazian <160056325+ashkanpazian@users.noreply.github.com>
2024-02-16 22:19:06 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,5 +134,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL ='home'
+
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
